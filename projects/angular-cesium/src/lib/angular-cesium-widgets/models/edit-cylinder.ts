7ddb38b07ac9f6ec2c22d5fd0c44f9829c622c86
Tom Povey <Tom.Povey@ghd.com>
2021-05-24 10:50:14 +0100

    added cylinder and wall ts models

---
+++ projects/angular-cesium/src/lib/angular-cesium-widgets/models/edit-cylinder.ts
@@ -0,0 +1,92 @@
+import { AcEntity } from '../../angular-cesium/models/ac-entity';
+import { Cartesian3 } from '../../angular-cesium/models/cartesian3';
+import { CylinderProps } from './cylinder-edit-options';
+
+export class EditCylinder extends AcEntity {
+  static counter = 0;
+  private id: string;
+  private editedEntityId: string;
+  private _show = true;
+  private _length: number;
+  private _cylinderProps: CylinderProps;
+  private _topRadius: number;
+  private _bottomRadius: number;
+  private _position: Cartesian3;
+
+  constructor(entityId: string, position: Cartesian3, topRadius: number, bottomRadius: number, length: number, cylinderProps?: CylinderProps) {
+    super();
+    this.editedEntityId = entityId;
+    this._length = length;
+    this.id = this.generateId();
+    this._cylinderProps = {...cylinderProps};
+    this._topRadius = topRadius;
+    this._bottomRadius = bottomRadius;
+    this._position = position;
+  }
+
+  get props(): CylinderProps {
+    return this._cylinderProps;
+  }
+  set props(value: CylinderProps) {
+    this._cylinderProps = value;
+  }
+
+  get show() {
+    return this._show;
+  }
+  set show(value) {
+    this._show = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+  set length(value) {
+    this._length = value;
+  }
+
+  get topRadius(): number {
+    return this._topRadius;
+  }
+  set topRadius(value: number) {
+    this._topRadius = value;
+  }
+
+  get bottomRadius(): number {
+    return this._bottomRadius;
+  }
+  set bottomRadius(value: number) {
+    this._bottomRadius = value;
+  }
+
+  get position(): Cartesian3 {
+    return this._position;
+  }
+  set position(value: Cartesian3) {
+    this._position = value;
+  }
+
+  getEditedEntityId(): string {
+    return this.editedEntityId;
+  }
+
+  getPosition(): Cartesian3 {
+    return this.position.clone();
+  }
+
+  getPositionCallback(): Cartesian3 {
+    return new Cesium.CallbackProperty(this.getPosition.bind(this), false);
+  }
+
+  setPositions(position: Cartesian3) {
+    this.position = position;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  private generateId(): string {
+    return 'edit-cylinder-' + EditCylinder.counter++;
+  }
+}
---
+++ projects/angular-cesium/src/lib/angular-cesium-widgets/models/edit-wall.ts
@@ -0,0 +1,91 @@
+import { WallProps } from './wall-edit-options';
+import { AcEntity } from '../../angular-cesium/models/ac-entity';
+import { Cartesian3 } from '../../angular-cesium/models/cartesian3';
+
+export class EditWall extends AcEntity {
+  static counter = 0;
+  private id: string;
+  private editedEntityId: string;
+  private positions: Cartesian3[];
+  private _show = true;
+  private _height: number;
+  private _wallProps: WallProps;
+
+  constructor(entityId: string, positions: Cartesian3[], height: number, wallProps?: WallProps) {
+    super();
+    this.editedEntityId = entityId;
+    this.positions = positions;
+    this._height = height;
+    this.id = this.generateId();
+    this._wallProps = {...wallProps};
+  }
+
+  get props(): WallProps {
+    return this._wallProps;
+  }
+
+  set props(value: WallProps) {
+    this._wallProps = value;
+  }
+
+  get show() {
+    return this._show;
+  }
+
+  set show(value) {
+    this._show = value;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  set height(value) {
+    this._height = value;
+  }
+
+  getEditedEntityId(): string {
+    return this.editedEntityId;
+  }
+
+  getPositions(): any[] {
+    return this.positions.map(p => p.clone());
+  }
+
+  getWallPositionsCallback() {
+    return new Cesium.CallbackProperty(this.getPositions.bind(this), false);
+  }
+
+  getPositionsMinHeight(): number[] {
+    return this.getPositions().map(pos => 
+      Cesium.Cartographic.fromCartesian(pos).height);
+  }
+
+  getPositionsMinHeightCallback() {
+    return new Cesium.CallbackProperty(this.getPositionsMinHeight.bind(this), false);
+  }
+
+  getPositionsMaxHeight(): number[] {
+    const positions = this.getPositionsMinHeight();
+    const maxHeight = positions
+      .reduce((prev, next) => prev > next ? prev : next, 0)
+      + this.height;
+    return Array(positions.length).fill(maxHeight);
+  }
+
+  getPositionsMaxHeightCallback() {
+    return new Cesium.CallbackProperty(this.getPositionsMaxHeight.bind(this), false);
+  }
+
+  setPositions(positions: Cartesian3[]) {
+    this.positions = positions;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  private generateId(): string {
+    return 'edit-wall-' + EditWall.counter++;
+  }
+}
